f1b9c6361c322666403cd7619ee85e010acf1516
TheofficialVbear <vebglims@hotmail.com>
2024-05-28 22:02:48 +0200

    fixed

--- Schoolsys.c
+++ Schoolsys.c
@@ -1,112 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-#define BOARD_SIZE 100
-
-// Function declarations
-void printBoard(int playerPosition);
-int rollDice();
-int movePlayer(int playerPosition);
-int checkSnakesAndLadders(int position);
-
-// Snakes and Ladders positions
-const int snakes[][2] = {
-    {16, 6},
-    {47, 26},
-    {49, 11},
-    {56, 53},
-    {62, 19},
-    {64, 60},
-    {87, 24},
-    {93, 73},
-    {95, 75},
-    {98, 78}
-};
-
-const int ladders[][2] = {
-    {1, 38},
-    {4, 14},
-    {9, 31},
-    {21, 42},
-    {28, 84},
-    {36, 44},
-    {51, 67},
-    {71, 91},
-    {80, 100}
-};
+void introtext();
+int UserChoice();
+void determineWinner(int UserChoice);
 
 int main() {
-    srand(time(0)); // Seed the random number generator
 
-    int playerPosition = 0;
-
-    printf("Welcome to Snakes and Ladders!\n");
-
-    while (playerPosition < BOARD_SIZE) {
-        printf("\nPress Enter to roll the dice...");
-        getchar();
-
-        int dice = rollDice();
-        printf("You rolled a %d!\n", dice);
-
-        playerPosition = movePlayer(playerPosition + dice);
-        printf("You moved to position %d\n", playerPosition);
-
-        playerPosition = checkSnakesAndLadders(playerPosition);
-        printBoard(playerPosition);
-
-        if (playerPosition == BOARD_SIZE) {
-            printf("Congratulations! You've reached the end and won the game!\n");
-            break;
-        }
-    }
-
+   introtext();
+   int userchoice = UserChoice();
+   determineWinner(userchoice);
+    
     return 0;
 }
 
-void printBoard(int playerPosition) {
-    for (int i = 1; i <= BOARD_SIZE; i++) {
-        if (i == playerPosition) {
-            printf("[P]");
-        } else {
-            printf("[%d]", i);
-        }
-
-        if (i % 10 == 0) {
-            printf("\n");
-        } else {
-            printf(" ");
-        }
-    }
+void introtext() {
+   printf("Welcome, what would you like to do?\n");
+   printf("1) Add to the list\n");
+   printf("2) Edit a user\n");
+   printf("3) Delete a user\n");
+   printf("3) Delete a user\n");
+   printf("4) Print list\n");
+   printf("5) Show information from ID\n");
+   printf("6) Exit the program\n");
+   
 }
 
-int rollDice() {
-    return (rand() % 6) + 1;
+int UserChoice() {
+    int choice;
+    scanf("%d", &choice);
+    while (choice < 1 || choice > 6) {
+        printf("Invalid choice. Please enter a number between 1 and 5: ");
+        scanf("%d", &choice);
+    }
+    return choice;
 }
 
-int movePlayer(int playerPosition) {
-    if (playerPosition > BOARD_SIZE) {
-        return BOARD_SIZE - (playerPosition - BOARD_SIZE);
-    } else {
-        return playerPosition;
+void determineWinner(int UserChoice) {
+    if (UserChoice == 1) {
+        printf("You chose 1\n");
+    }else if(UserChoice == 2) {
+        printf("You chose 2\n");
+    }else if(UserChoice == 3) {
+        printf("You chose 3\n");
+    }else if(UserChoice == 4) {
+        printf("You chose 4\n");
+    }else if(UserChoice == 5) {
+        printf("You chose 5\n");
+    }else if(UserChoice == 6) {
+        printf("You chose 6\n");
+        
+    }else {
+        printf("how??");
     }
-}
-
-int checkSnakesAndLadders(int position) {
-    for (int i = 0; i < sizeof(snakes) / sizeof(snakes[0]); i++) {
-        if (position == snakes[i][0]) {
-            printf("Oops! You landed on a snake. Sliding down from %d to %d.\n", snakes[i][0], snakes[i][1]);
-            return snakes[i][1];
-        }
-    }
-
-    for (int i = 0; i < sizeof(ladders) / sizeof(ladders[0]); i++) {
-        if (position == ladders[i][0]) {
-            printf("Yay! You landed on a ladder. Climbing up from %d to %d.\n", ladders[i][0], ladders[i][1]);
-            return ladders[i][1];
-        }
-    }
-
-    return position;
 }
