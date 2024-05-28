edcb176c938e3dc7dacd3b4e57a7cd483706fc10
TheofficialVbear <vebglims@hotmail.com>
2024-05-28 23:06:03 +0200

    Worked further on a system

--- Schoolsys.c
+++ Schoolsys.c
@@ -1,16 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+#define MAX_NAME_LENGTH 50
+#define MAX_STUDENTS 3
+
+typedef struct {
+    int id;
+    char name[MAX_NAME_LENGTH];
+    int age;
+    char grade;
+} Student;
+
+
+void printStudents(Student students[], int numStudents);
 void introtext();
 int UserChoice();
-void determineWinner(int UserChoice);
+void pickanum();
+
+// Initialize the list of students
+Student students[MAX_STUDENTS] = {
+    {1, "John Doe", 20, 'A'},
+    {2, "Jane Smith", 21, 'B'},
+    {3, "Bob Johnson", 19, 'A'}
+};
+
+int numStudents = MAX_STUDENTS;
 
 int main() {
 
-   introtext();
-   int userchoice = UserChoice();
-   determineWinner(userchoice);
-    
+   pickanum();
+   
     return 0;
 }
 
@@ -25,30 +45,45 @@ void introtext() {
    
 }
 
-int UserChoice() {
-    int choice;
-    scanf("%d", &choice);
-    while (choice < 1 || choice > 6) {
-        printf("Invalid choice. Please enter a number between 1 and 6: ");
-        scanf("%d", &choice);
+void printStudents(Student students[], int numStudents) {
+    printf("List of students:\n");
+    for (int i = 0; i < numStudents; i++) {
+        printf("ID: %d, Name: %s, Age: %d, Grade: %c\n", students[i].id, students[i].name, students[i].age, students[i].grade);
     }
-    return choice;
 }
 
-void determineWinner(int UserChoice) {
-    if (UserChoice == 1) {
-        printf("You chose 1\n");
-    }else if(UserChoice == 2) {
-        printf("You chose 2\n");
-    }else if(UserChoice == 3) {
-        printf("You chose 3\n");
-    }else if(UserChoice == 4) {
-        printf("You chose 4\n");
-    }else if(UserChoice == 5) {
-        printf("You chose 5\n");
-    }else if(UserChoice == 6) {
-        printf("You chose 6\n");
-    }else {
-        printf("how??");
+void pickanum() {
+    while (1){
+        introtext();
+        int choice;
+        scanf("%d", &choice);
+        while (choice < 1 || choice > 6) {
+            printf("Invalid choice. Please enter a number between 1 and 6: ");
+            scanf("%d", &choice);
+        }
+        if (choice == 1) {
+            printf("You chose 1\n");
+        }else if(choice == 2) {
+            printf("You chose 2\n");
+        }else if(choice == 3) {
+            printf("You chose 3\n");
+        }else if(choice == 4) {
+            printf("You chose 4\n");
+            printStudents(students, numStudents);
+        }else if(choice == 5) {
+            printf("You chose 5\n");
+        }else if(choice == 6) {
+            printf("Exiting program...\n");
+            break;
+        }else {
+            printf("how??");
+            break;
+        }
     }
 }
+
+
+
+
+
+
